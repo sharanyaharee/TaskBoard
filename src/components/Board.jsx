6f384850423120c6eed23a3d76316b121f45832e
Sharanya <sharu.sesh@gmail.com>
2023-04-17 09:38:34 +0400

    Final code

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -13,29 +13,29 @@ const Board = ({  // Props well received on Board Component ;)
     setCompletedItemList(deletedList); //  once both the index values found equal Filter function exits and the (rest of the ) unmatched index Tasks are stored to a new array
   };//  on exit of filter function new Array is stored to updated completed Task State using its setter Function
  
-  const updatePendingStatus = (id, newStatus) => { // function defined to move Pending tasks to Ongoing Tasks state
+  const updatePendingStatus = (item, newStatus) => { // function defined to move Pending tasks to Ongoing Tasks state
     let moveItems = pendingItems; // assigning Pending Task Array to a temp array
     moveItems = moveItems.map((items) => { // mapping the temp array.
-      if (items.id === id) {  // once the array item id matches with the id of the item to be moved (ie. id which is received as function parameter) 
-        items.category = newStatus; // new status "ongoing" is assigned to the category key value
+      if (items.task === item) {  // once the array item id matches with the id of the item to be moved (ie. id which is received as function parameter) 
+        items.category = newStatus; // new status "ongoing" is assigned to the category key's value
         setOngoingItemList([...ongoingItems, items]); // updating the ongoing task state by adding new task to the existing ongoing tasks by spreading the ongoing tasks array
       }
-      const remainingPendingList = pendingItems.filter((id) => {
-        return i !== id;
+      const remainingPendingList = pendingItems.filter((item) => {
+        return item.task !== items;
       }); //  removing the moved task from Pending task array by sorting it out with filter function
-      setPendingItemList(remainingPendingList); // updating pending list item with remaining Array returned by filter function
+      setPendingItemList(remainingPendingList); // updating pending list item with remaining task Array returned by filter function
     });
   };
 
-  const updateOngoingStatus = (id, newStatus) => {  // function defined to move ongoing tasks to completed Tasks state
+  const updateOngoingStatus = (item, newStatus) => {  // function defined to move ongoing tasks to completed Tasks state
     let moveItems = ongoingItems; // assigning ongoing Task Array to a temp array
     moveItems = moveItems.map((items) => { // mapping the temp array.
-      if (items.id === id) {  // once the array item id matches with the id of the item to be moved (ie. id which is received as function parameter) 
-        items.category = newStatus; // new status "completed" is assigned to the category key value
-        setCompletedItemList([...completedItems, items]);  // updating the completed task state by adding newly moved task to the existing completed tasks by spreading the ongoing tasks array
+      if (items.task === item) {  // once the array item.task matches with the item task to be moved 
+        items.category = newStatus; // new status "completed" is assigned to the category key's value
+        setCompletedItemList([...completedItems, items]);  // updating the completed task state by adding newly moved task to the existing completed tasks by spreading the completed tasks array
       }
-      const remainingOngoingList = ongoingItems.filter((id) => {
-        return i !== id;
+      const remainingOngoingList = ongoingItems.filter((item) => {
+        return item.task !== items;
       });  //  removing the moved task from ongoing task array by sorting it out with filter function
       setOngoingItemList(remainingOngoingList);// updating  ongoing task list item with remaining Array returned by filter function
     });
@@ -53,7 +53,7 @@ const Board = ({  // Props well received on Board Component ;)
               <p>{item.task}</p>
               <button
                 className="taskButton move"
-                onClick={() => updatePendingStatus(item.id, "ongoing")} > {/* Button onClick evokes the updatePendingStatus function, passing task id and Status message as arguments */}
+                onClick={() => updatePendingStatus(item.task, "ongoing")} > {/* Button onClick evokes the updatePendingStatus function, passing task name and Status message as arguments */}
                 Move
               </button>
             </div>
@@ -70,7 +70,7 @@ const Board = ({  // Props well received on Board Component ;)
               <p>{item.task}</p>
               <button
                 className="taskButton move"
-                onClick={() => updateOngoingStatus(item.id, "completed")}>  {/* Button onClick evokes the updateOngoingStatus function, passing task id and status message as arguments */}
+                onClick={() => updateOngoingStatus(item.task, "completed")}>  {/* Button onClick evokes the updateOngoingStatus function, passing task name and status message as arguments */}
                 Move
               </button>
             </div>
